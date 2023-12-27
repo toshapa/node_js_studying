c1dd0512efea87b1996b29bd89ff65865aa4b9ef
AP <tonichok@i.ua>
2023-12-27 15:31:55 +0200

    trying to understand how event loop worksundestood

---
+++ 002_blocking_event_loop/fibonacci_with_cache.js
@@ -0,0 +1,15 @@
+setTimeout( () => console.log('TIMER'), 0)
+
+const fibonacci = (n) => {
+    return new Promise((resolve, reject) => {
+        if ( n === 0 ||  n === 1) {
+            return resolve(n)
+        }
+        setImmediate(() => {
+            Promise.all([fibonacci(n - 1), fibonacci(n - 2)])
+                .then(([fibonacci1, fibonacci2]) => {resolve(fibonacci1 + fibonacci2)})
+        })
+    })
+}
+
+fibonacci(1).then((res) => console.log(res,  performance.now().toFixed(2)))
