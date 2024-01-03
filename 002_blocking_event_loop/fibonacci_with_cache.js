f8e6e0203f175a2c7b36a4904f6f5d03c68a2ec0
AP <tonichok@i.ua>
2024-01-03 14:08:11 +0200

    resume learning node.js

--- 002_blocking_event_loop/fibonacci_with_cache.js
+++ 002_blocking_event_loop/fibonacci_with_cache.js
@@ -1,15 +1,29 @@
+
+
 setTimeout( () => console.log('TIMER'), 0)
+
+const cache = new Map()
 
 const fibonacci = (n) => {
     return new Promise((resolve, reject) => {
         if ( n === 0 ||  n === 1) {
             return resolve(n)
         }
+        if (cache.has(n)) {
+            return resolve(cache.get(n))
+        }
         setImmediate(() => {
-            Promise.all([fibonacci(n - 1), fibonacci(n - 2)])
-                .then(([fibonacci1, fibonacci2]) => {resolve(fibonacci1 + fibonacci2)})
+            fibonacci(n-1).then((fib1) => fibonacci(n-2).then((fib2) => {
+                cache.set(n, fib1+fib2)
+                resolve(fib1 + fib2)
+            }))
+            })
+            // Promise.all([fibonacci(n - 1), fibonacci(n - 2)])
+            //     .then(([fibonacci1, fibonacci2]) => {
+            //         cache.set(n, fibonacci1+fibonacci2)
+            //         resolve(fibonacci1 + fibonacci2)
+            //     })
         })
-    })
-}
+    }
 
-fibonacci(1).then((res) => console.log(res,  performance.now().toFixed(2)))
+fibonacci(150).then((res) => console.log(res,  performance.now().toFixed(2)))
