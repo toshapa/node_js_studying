b4a5c1af6cc3b36691a694b7a273b9bf8094d65d
AP <tonichok@i.ua>
2024-01-04 12:51:09 +0200

    added a fibonacci linear complexity

---
+++ 002_blocking_event_loop/fibonacci_linear_complexity.js
@@ -0,0 +1,31 @@
+const timeStamp = () => {
+    return performance.now().toFixed(2)
+}
+
+function info (text) {
+    console.log(text, timeStamp())
+}
+
+setTimeout(() => info('Timer'), 10)
+
+info('Programm start')
+
+function fib(n) {
+    if (n === 0 || n === 1) {
+        return n
+    }
+    let fib1 = 0
+    let fib2 = 1
+    let sum
+    for  (let i = 1; i < n; i++) {
+        sum = fib1 + fib2;
+        fib1 = fib2
+        fib2 = sum
+    }
+    return sum
+}
+
+console.log(fib(150))
+
+info('Programm end')
+
